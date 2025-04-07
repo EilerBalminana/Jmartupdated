c816d623ef883b888289a3430e42e8b0134cd1ea
EilerBalminana <154731433+EilerBalminana@users.noreply.github.com>
2025-04-07 11:30:06 +0300

    Add files via upload

---
+++ sql/jmartdb04072025.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2025 at 10:27 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jmartdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adjusted_products`
+--
+-- Error reading structure for table jmartdb.adjusted_products: #1932 - Table &#039;jmartdb.adjusted_products&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.adjusted_products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`adjusted_products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adjustments`
+--
+-- Error reading structure for table jmartdb.adjustments: #1932 - Table &#039;jmartdb.adjustments&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.adjustments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`adjustments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table jmartdb.categories: #1932 - Table &#039;jmartdb.categories&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`categories`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `currencies`
+--
+-- Error reading structure for table jmartdb.currencies: #1932 - Table &#039;jmartdb.currencies&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.currencies: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`currencies`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+-- Error reading structure for table jmartdb.customers: #1932 - Table &#039;jmartdb.customers&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.customers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`customers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+-- Error reading structure for table jmartdb.expenses: #1932 - Table &#039;jmartdb.expenses&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.expenses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`expenses`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expense_categories`
+--
+-- Error reading structure for table jmartdb.expense_categories: #1932 - Table &#039;jmartdb.expense_categories&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.expense_categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`expense_categories`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table jmartdb.failed_jobs: #1932 - Table &#039;jmartdb.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+-- Error reading structure for table jmartdb.media: #1932 - Table &#039;jmartdb.media&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.media: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`media`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table jmartdb.migrations: #1932 - Table &#039;jmartdb.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_permissions`
+--
+-- Error reading structure for table jmartdb.model_has_permissions: #1932 - Table &#039;jmartdb.model_has_permissions&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.model_has_permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`model_has_permissions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_roles`
+--
+-- Error reading structure for table jmartdb.model_has_roles: #1932 - Table &#039;jmartdb.model_has_roles&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.model_has_roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`model_has_roles`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table jmartdb.password_resets: #1932 - Table &#039;jmartdb.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+-- Error reading structure for table jmartdb.permissions: #1932 - Table &#039;jmartdb.permissions&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`permissions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table jmartdb.products: #1932 - Table &#039;jmartdb.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+-- Error reading structure for table jmartdb.purchases: #1932 - Table &#039;jmartdb.purchases&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.purchases: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`purchases`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_details`
+--
+-- Error reading structure for table jmartdb.purchase_details: #1932 - Table &#039;jmartdb.purchase_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.purchase_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`purchase_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_payments`
+--
+-- Error reading structure for table jmartdb.purchase_payments: #1932 - Table &#039;jmartdb.purchase_payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.purchase_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`purchase_payments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_returns`
+--
+-- Error reading structure for table jmartdb.purchase_returns: #1932 - Table &#039;jmartdb.purchase_returns&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.purchase_returns: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`purchase_returns`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_return_details`
+--
+-- Error reading structure for table jmartdb.purchase_return_details: #1932 - Table &#039;jmartdb.purchase_return_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.purchase_return_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`purchase_return_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_return_payments`
+--
+-- Error reading structure for table jmartdb.purchase_return_payments: #1932 - Table &#039;jmartdb.purchase_return_payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.purchase_return_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`purchase_return_payments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quotations`
+--
+-- Error reading structure for table jmartdb.quotations: #1932 - Table &#039;jmartdb.quotations&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.quotations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`quotations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quotation_details`
+--
+-- Error reading structure for table jmartdb.quotation_details: #1932 - Table &#039;jmartdb.quotation_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.quotation_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`quotation_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+-- Error reading structure for table jmartdb.roles: #1932 - Table &#039;jmartdb.roles&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`roles`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_has_permissions`
+--
+-- Error reading structure for table jmartdb.role_has_permissions: #1932 - Table &#039;jmartdb.role_has_permissions&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.role_has_permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`role_has_permissions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+-- Error reading structure for table jmartdb.sales: #1932 - Table &#039;jmartdb.sales&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.sales: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`sales`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_details`
+--
+-- Error reading structure for table jmartdb.sale_details: #1932 - Table &#039;jmartdb.sale_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.sale_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`sale_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_payments`
+--
+-- Error reading structure for table jmartdb.sale_payments: #1932 - Table &#039;jmartdb.sale_payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.sale_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`sale_payments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_returns`
+--
+-- Error reading structure for table jmartdb.sale_returns: #1932 - Table &#039;jmartdb.sale_returns&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.sale_returns: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`sale_returns`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_return_details`
+--
+-- Error reading structure for table jmartdb.sale_return_details: #1932 - Table &#039;jmartdb.sale_return_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.sale_return_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`sale_return_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_return_payments`
+--
+-- Error reading structure for table jmartdb.sale_return_payments: #1932 - Table &#039;jmartdb.sale_return_payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.sale_return_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`sale_return_payments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+-- Error reading structure for table jmartdb.settings: #1932 - Table &#039;jmartdb.settings&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.settings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`settings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+-- Error reading structure for table jmartdb.suppliers: #1932 - Table &#039;jmartdb.suppliers&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.suppliers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`suppliers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `units`
+--
+-- Error reading structure for table jmartdb.units: #1932 - Table &#039;jmartdb.units&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.units: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`units`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploads`
+--
+-- Error reading structure for table jmartdb.uploads: #1932 - Table &#039;jmartdb.uploads&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.uploads: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`uploads`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table jmartdb.users: #1932 - Table &#039;jmartdb.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table jmartdb.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jmartdb`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
